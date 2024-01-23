fc81c5fdff845a12552e5bb20e16555a2726639b
Malik Amir Hamza <125199922+MalikAmirHamza@users.noreply.github.com>
2024-01-23 19:27:50 +0500

    audio.js

--- audio.js
+++ audio.js
@@ -13,8 +13,8 @@ let songIndex = 2 ;
 loadsong(songs[songIndex])
 function loadsong(song){
   title.innerText = song;
-  audio.src = `/music/${song}.mp3`;
-  cover.src = `/images/${song}.jpg` 
+  audio.src = `./music/${song}.mp3`;
+  cover.src = `./images/${song}.jpg` 
 }
 
 function playmusic(){
@@ -51,12 +51,7 @@ function update(e){
     const {duration , currentTime} = e.srcElement;
     const percent = (currentTime / duration) * 100;
     progress.style.width = `${percent}%`;
-
-
 }
-
-
-
 playBtn.addEventListener('click', ()=>{
 const isplaying = musicContainer.classList.contains('play');
 if(isplaying){
@@ -70,80 +65,3 @@ else{
 prevBtn.addEventListener('click' , PrevSong);
 nextBtn.addEventListener('click' , nextSong);
 audio.addEventListener('timeupdate' , update)
-
-
-
-
-
-
-
-
-// // songs title
-// const songs = ['hey' , 'summer' , 'ukulele'];
-// // Tract the Song
-// let songIndex = 2;
-// //initially Load songs
-// loadsong(songs[songIndex]);
-
-// function loadsong(song){
-//     title.innerText = song;
-//     audio.src = `/music/${song}.mp3`;
-//     cover.src = `/images/${song}.jpg`;
-// }
-// function playmusic(){
-//     musicContainer.classList.add('play');
-//     playBtn.querySelector('i.fas').classList.remove('fa-play');
-//     playBtn.querySelector('i.fas').classList.add('fa-pause');
-//     audio.play()
-
-// }
-// function stopmusic(){
-//     musicContainer.classList.remove('play');
-//     playBtn.querySelector('i.fas').classList.remove('fa-pause');
-//     playBtn.querySelector('i.fas').classList.add('fa-play');
-//     audio.pause()
-    
-
-// }
-
-// function PrevSong(){
-//     songIndex--;
-//     if(songIndex < 0){
-//         songIndex = songs.length-1;
-//     }
-//     console.log(songIndex)
-//     loadsong(songs[songIndex]);
-//     playmusic();
-// }
-// function nextSong(){
-//     console.log(1)
-//     songIndex++;
-//     if(songIndex > songs.length-1){
-//         songIndex = 0;
-//     }
-//     loadsong(songs[songIndex]);
-//     playmusic();
-// }
-// function update(e){
-//     const {duration , currentTime}  = e.srcElement;
-//     const progresspercent = (currentTime / duration) * 100;
-//     progress.style.width  = `${progresspercent}%` 
-// }
-
-// playBtn.addEventListener('click' , function(){
-//     const isplaying = musicContainer.classList.contains('play');
-//     if(isplaying){
-//         stopmusic();
-//     }
-//     else{
-//         playmusic()
-//     }
-//  })
-
-
-//  prevBtn.addEventListener('click' , PrevSong);
-//  nextBtn.addEventListener('click' , nextSong);
-//  audio.addEventListener('timeupdate' , update)
-
-
-
